e5bc181448aea1e2ef273fdf2c8a071b77958674
Jake Malis <jakebmalis@gmail.com>
2020-01-25 14:15:03 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/January11SimpleAuto.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class January11SimpleAuto extends LinearOpMode {
+    public DcMotor leftMotor1;
+    public DcMotor rightMotor1;
+    public DcMotor leftMotor2;
+    public DcMotor rightMotor2;
+    public CRServo clawMotor;
+    public DcMotor armMotor;
+
+    @Override
+    public void runOpMode(){
+        leftMotor1 = hardwareMap.get(DcMotor.class, "leftMotor1");
+        leftMotor2 = hardwareMap.get(DcMotor.class, "leftMotor2");
+        rightMotor1 = hardwareMap.get(DcMotor.class, "rightMotor1");
+        rightMotor2 = hardwareMap.get(DcMotor.class, "rightMotor2");
+        clawMotor = hardwareMap.get(CRServo.class, "clawMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "arm_motor");
+
+        leftMotor1.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor2.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor1.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor2.setDirection(DcMotor.Direction.FORWARD);
+        armMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        leftMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+
+        waitForStart();
+        if(opModeIsActive()) {
+
+            leftMotor1.setPower(-0.5);
+            leftMotor2.setPower(-0.5);
+            rightMotor1.setPower(-0.5);
+            rightMotor2.setPower(-0.5);
+            sleep(600
+            );
+
+            leftMotor1.setPower(0);
+            leftMotor2.setPower(0);
+            rightMotor1.setPower(0);
+            rightMotor2.setPower(0);
+
+
+        }
+    }
+}
